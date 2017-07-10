580cd3c540a6fe1596e51b09058eb76a2f520236
bankincity <bank.t.2002@gmail.com>
2017-07-10 17:02:04 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,7 @@
 <?php
-echo "I am a bot";
+
+curl -X GET \
+-H 'Authorization: Bearer {F2bejKi+Zd2+2VIs9YrRAt4sctvtJv6MMDlPN6kTh9eo1Xoh/Lq8021aEtL0yDk7yh5HYkaQEbvM52DJR7yNt162DM5dyg0qPkdZneUrAX1mrgkxaP3KDee0CnhdZARnowHeuggOVRJI7L98Knm7PwdB04t89/1O/w1cDnyilFU=}' \
+https://api.line.me/v1/oauth/verify
+
 ?>
